f9cf3bf46353b899dabe26f683cc77f3b6ec3b98
RAIYMBEK <raisrais002@gmail.com>
2024-05-24 14:59:06 +0500

    full functional login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { BrowserRouter, Route, Routes, Navigate} from "react-router-dom"
 import "./App.css"
+import ErrorPage from './routes/not-found/error-page.jsx';
+import Login_page from './routes/login-page/login-page.jsx';
+import Home from './routes/home/home.jsx';
+import { useAuthContext } from "./hooks/useAuthContext.jsx";
+
+
 
 export default function App() {
 
+  const {user} = useAuthContext()
+
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route 
+            path="/login_page"
+            element={!user ? <Login_page/> : <Navigate to="/home"/>}
+          />
+          <Route 
+            path="/home"
+            element={user ? <Home/> : <Navigate to="/login_page"/>}
+          />
+          <Route
+            path="*"
+            element={<ErrorPage/>}
+          />
+
+        
+        </Routes>
+      
+      </BrowserRouter> 
     </div>
 
   )
--- src/main.jsx
+++ src/main.jsx
@@ -9,23 +9,13 @@ import './index.css'
 import ErrorPage from './routes/not-found/error-page.jsx'
 import Login_page from './routes/login-page/login-page.jsx'
 import Home from './routes/home/home.jsx'
+import { AuthContext, AuthContextProvider } from './context/authContext.jsx'
 
-const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Login_page/>,
-      errorElement: <ErrorPage/>,
-    },
-
-    {
-        path: "/home",
-        element: <Home/>,
-        errorElement: <ErrorPage/>,
-      },
-  ]);
 
   ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </React.StrictMode>
   );
